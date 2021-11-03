e24bbe5224b6a108bf30a0e0e47a7bbd807384e3
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2021-11-02 22:24:06 -0400

    added instructions and operator comments

--- regex.lisp
+++ regex.lisp
@@ -1,31 +1,57 @@
 (in-package :regex)
 ;; TODO: make better flow charts https://asciiflow.com/#/
 
-;; match instructions
-;; char literal 'a'
-;; >-a-> 
+#|
+MATCH FLOW/OPERATORS
 
-;; concatenation of two sub expressions (implicit)
-;; >-|e1|--|e2|-> 
+char literal 'a'
+>-a-> 
 
-;; one sub expression or another '|'
-;;   |-|e1|->
-;; >--
-;;   |-|e1|->
+concatenation of two sub expressions (implicit)
+>-|e1|--|e2|-> 
 
-;; zero or one of sub expression '?'
-;;   |-|e1|->
-;; >--
-;;   |---->
+one sub expression or another '|'
+  |-|e1|->
+>--
+  |-|e1|->
 
-;; zero or more of sub expression '*'
-;;   |e1|-|
-;; >-o<---|
-;;   |------->
+zero or one of sub expression '?'
+  |-|e1|->
+>--
+  |---->
 
-;; one or more of sub expression '+'
-;;   <----|
-;; >-|e1|-o
+zero or more of sub expression '*'
+  |e1|-|
+>-o<---|
+  |------->
 
-;; starting with instructions
-;; build them up and come up with new ones when needed
+one or more of sub expression '+'
+  <----|
+>-|e1|-o
+|#
+
+#|
+INSTRUCTIONS:
+MATCH: made it to the end of a nfa
+LITERAL a: match to character a
+JUMP n: jump to instruction n
+SPLIT a b: execute both a and b
+|#
+
+#|
+OPERATORS->INSTRUCTIONS
+next is the next instruction, this is the current instruction
+char -> (lit #\a)
+e1e2 -> (e1 e2)
+e1|e2 -> (split (e1 (jump next) e2))
+e? -> (split e next)
+e* -> (split (e (jump this) next))
+e+ -> (e (split this next))
+
+EX: r"a+b+"
+(a (split
+     this       instruction 0
+     (b (split
+         this   instruction 2
+         +match+))))
+|#
